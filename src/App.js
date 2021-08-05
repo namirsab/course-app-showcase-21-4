9ad891cdb9ca12e75e27750690e0d161432a2037
Namir Sayed-Ahmad Baraza <namirsab@gmail.com>
2021-08-05 11:15:53 +0200

    Make AppHeader component. Header renders different title depending on the path

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
+import "./App.css";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation";
-import "./App.css";
 import CoursesPage from "./pages/CoursesPage";
+import StudentsPage from "./pages/StudentsPage";
+import AppHeader from "./components/AppHeader";
 
 function App() {
   return <div className="App">
-    <header className="App__header">
-      Header
-    </header>
+    <AppHeader />
     <main className="App__content">
     <Switch>
       <Route path="/courses">
         <CoursesPage />
       </Route>
       <Route path="/students">
-        Students
+        <StudentsPage />
       </Route>
       <Route path="/">
         <Redirect to="/courses" />
