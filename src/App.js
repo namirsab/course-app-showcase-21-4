adc2cad4ddb12ab795f18cb8945babab52dff200
Namir Sayed-Ahmad Baraza <namirsab@gmail.com>
2021-08-05 10:45:55 +0200

    Setup basic routing with React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <div className="App">
+    <Switch>
+      <Route path="/courses">
+        Courses
+      </Route>
+      <Route path="/students">
+        Students
+      </Route>
+      <Route path="/">
+        <Redirect to="/courses" />
+      </Route>
+    </Switch>
+  </div>;
 }
 
 export default App;
